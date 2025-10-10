0fb11c945c83d231417c3f55e3a4d444e4d7d11f
Ojiyumm <125381866+Ojiyumm@users.noreply.github.com>
2025-10-10 17:28:07 +0800

    Update run_eval.sh
    save results

--- benchmarking/run_eval.sh
+++ benchmarking/run_eval.sh
@@ -11,4 +11,5 @@ export OPENAI_API_KEY="EMPTY"
 
 lm_eval --model openai-completions \
     --model_args base_url="http://192.168.0.82:8000/v1/completions",model=davinci-002,tokenized_requests=false \
-    --tasks lambada_openai
+    --tasks lambada_openai \
+    --output_path ./eval_results 
